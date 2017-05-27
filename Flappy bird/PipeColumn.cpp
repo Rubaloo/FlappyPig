34459c429226347a978906dc1e6b10774171f061
Ruben Salvador <ru.bcn90@gmail.com>
2017-05-27 12:09:19 +0200

    PipeColumn class added

---
+++ Flappy bird/PipeColumn.cpp
@@ -0,0 +1,41 @@
+//
+//  PipeColumn.cpp
+//  Flappy bird
+//
+//  Created by Ruben on 27/5/17.
+//  Copyright © 2017 Ruben. All rights reserved.
+//
+
+
+#include "PipeColumn.hpp"
+
+
+PipeColumn::PipeColumn(){}
+PipeColumn::PipeColumn(int xCenter) {
+    float uPipeHeight = rand() % 200 + 100;
+    float dPipeHeight = SCREEN_HEIGHT - uPipeHeight - K_COLUMN_PIPES_SPACE;
+    
+    Box uPipeBox(kmVec3Make(xCenter, uPipeHeight/2.0, 0.0), kmSizeMake(K_COLUMN_PIPES_WIDTH, uPipeHeight));
+    Box dPipeBox(kmVec3Make(xCenter, SCREEN_HEIGHT - (dPipeHeight/2.0), 0.0), kmSizeMake(K_COLUMN_PIPES_WIDTH, dPipeHeight));
+    uPipeBox.setVelocity(kmVec3Make(k_COLUMN_SPEED, 0, 0));
+    dPipeBox.setVelocity(kmVec3Make(k_COLUMN_SPEED, 0, 0));
+    
+    up = new Pipe(uPipeBox);
+    down = new Pipe(dPipeBox);
+}
+
+bool PipeColumn::intersect(GameObject* gObject)
+{
+    return (up->intersect(gObject)) && (down->intersect(gObject));
+}
+
+void PipeColumn::update(float dt)
+{
+    up->update(dt);
+    down->update(dt);
+}
+
+void PipeColumn::render() {
+    up->render();
+    down->render();
+};
---
+++ Flappy bird/PipeColumn.hpp
@@ -0,0 +1,31 @@
+//
+//  PipeColumn.hpp
+//  Flappy bird
+//
+//  Created by Ruben on 27/5/17.
+//  Copyright © 2017 Ruben. All rights reserved.
+//
+
+#ifndef PipeColumn_hpp
+#define PipeColumn_hpp
+
+#include <stdio.h>
+#include "Pipe.hpp"
+
+#define K_COLUMN_PIPES_SPACE 150
+#define k_COLUMN_SPEED -100
+#define K_COLUMN_PIPES_WIDTH 50
+
+class PipeColumn : public GameObject {
+private:
+    Pipe* up;
+    Pipe* down;
+public:
+    PipeColumn();
+    PipeColumn(int xCenter);
+    
+    bool intersect(GameObject* gObject);
+    void update(float dt);
+    void render();
+};
+#endif /* PipeColumn_hpp */
