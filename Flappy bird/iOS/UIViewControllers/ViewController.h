901d81f09d6561b4531045884065e44cf0468c6b
Ruben Salvador <ruben.salvador.santiago@gmail.com>
2018-08-31 00:07:09 +0200

    comments deleted

--- Flappy bird/iOS/UIViewControllers/ViewController.h
+++ Flappy bird/iOS/UIViewControllers/ViewController.h
@@ -1,15 +1,5 @@
-//
-//  ViewController.h
-//  Flappy bird
-//
-//  Created by Ruben on 20/5/17.
-//  Copyright © 2017 Ruben. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-
-
 @end
 
--- Flappy bird/iOS/UIViewControllers/ViewController.m
+++ Flappy bird/iOS/UIViewControllers/ViewController.m
@@ -1,35 +1,25 @@
-//
-//  ViewController.m
-//  Flappy bird
-//
-//  Created by Ruben on 20/5/17.
-//  Copyright © 2017 Ruben. All rights reserved.
-//
-
 #import "ViewController.h"
 #import "OpenGLView.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet OpenGLView *glView;
-
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-  
-}
-
--(void) viewDidLayoutSubviews
+- (void)viewDidLoad
 {
-  [_glView setupGL];
+    [super viewDidLoad];
 }
 
-- (void)didReceiveMemoryWarning {
+- (void) viewDidLayoutSubviews
+{
+    [_glView setupGL];
+}
+
+- (void)didReceiveMemoryWarning
+{
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
